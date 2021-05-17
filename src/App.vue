b25be7165f289ea6958ee040496db915baa1cd89
nicolascampbell <nicodsm7@gmail.com>
2021-05-17 22:02:43 +0200

    added the navbar again for easier movement

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="container" class="vh-100">
+    <nav-bar/>
     <router-view class="view"></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
+import nav from './components/navbar.vue'
 export default Vue.extend({
   name: "app",
+  components:{
+    'nav-bar':nav,
+  },
   data(){
     return{
     }
