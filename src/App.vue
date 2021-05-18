c60575f808f7c04c4c23558a0eb7e809e9414539
nicolascampbell <nicodsm7@gmail.com>
2021-05-18 13:48:32 +0200

    Navigator like in Jupyter

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div id="container" class="vh-100">
-    <nav-bar/>
+    <nav-bar />
     <router-view class="view"></router-view>
+    <navigator id="navi" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
-import nav from './components/navbar.vue'
+import nav from "./components/navbar.vue"
+import navigator from "./components/navigator.vue"
 export default Vue.extend({
   name: "app",
-  components:{
-    'nav-bar':nav,
+  components: {
+    "nav-bar": nav,
+    navigator,
   },
-  data(){
-    return{
-    }
+  data() {
+    return {}
   },
-  methods:{
-    
-  }
+  methods: {},
 })
 </script>
 
 <style>
+#navi {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding: 0px 20px 10px 0px;
+  transform: scale(0.7);
+}
 </style>
---
+++ src/components/navigator.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="navigator">
+    <b-container class="bv-example-row">
+      <b-row align-h="center" >
+        <b-col cols="6">
+          <img
+            @click="handleClick('up')"
+            @mouseover="hover = true"
+            @mouseleave="hover = false"
+            class="active"
+            :src="require('../assets/arrow_up.png')"
+          />
+        </b-col>
+      </b-row>
+      <b-row align-h="between">
+          <b-col style="padding:0px 10px 0px 11px;" cols="2"
+          ><img
+            @click="handleClick('left')"
+            @mouseover="hover = true"
+            @mouseleave="hover = false"
+            class="active"
+            :src="require('../assets/arrow_left.png')"
+        /></b-col>
+        <b-col style="padding-left:11px;" cols="2">
+          <img
+            @click="handleClick('right')"
+            @mouseover="hover = true"
+            @mouseleave="hover = false"
+            class="active"
+            :src="require('../assets/arrow_right.png')"
+          />
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col cols="6"
+          ><img
+            @click="handleClick('down')"
+            @mouseover="hover = true"
+            @mouseleave="hover = false"
+            class="active"
+            :src="require('../assets/arrow_down.png')"
+        /></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({
+  name: "navigator",
+  data() {
+    return {
+      hover: false,
+    }
+  },
+  methods: {
+    handleClick(action: String) {
+      this.$router.push({
+        name: "",
+        params: {},
+      })
+    },
+  },
+})
+</script>
+<style>
+
+.active{
+    transform:scale(0.9);
+    transition: transform .2s;
+  }
+  .active:hover{
+    transform: scale(1);
+  }
+  .active:focus{
+    filter: blur(5px) brightness(90%);
+  }
+</style>
