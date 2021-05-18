02d7f5de0528198fdd31f1ca79b571430d92353e
nicolascampbell <nicodsm7@gmail.com>
2021-05-18 13:50:49 +0200

    temporal change to access film images with button

--- src/views/FilmList.vue
+++ src/views/FilmList.vue
@@ -1,30 +1,60 @@
 <template>
   <div id="films_container" class="container">
-     <b-row class="vh-100 text-center" align-v="center" v-for="film in films" :key="film.id">
-      <b-col cols="2">
-        <b-button :to="{ name: 'Home' }">
-          go Back! 
-        </b-button>
-      </b-col>
+    <b-row
+      class="vh-100 text-center"
+      align-v="center"
+      v-for="film in films"
+      :key="film.id"
+    >
       <b-col>
-        <film-card :filmInfo="film"/>
+        <film-card :filmInfo="film" />
       </b-col>
-      <b-col cols="1"></b-col>
-
-     </b-row>
+      <b-col cols="1">
+        <img
+          @click="goToFilm(film)"
+          @mouseover="hover = true"
+          @mouseleave="hover = false"
+          class="active"
+          :src="require('../assets/arrow_right.png')"
+        >
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script lang="ts">
-import filmCard from '../components/film_card.vue';
-import films from  "../films.json";
-export default ({
-  components:{
-      filmCard,
+import Vue from "vue"
+import filmCard from "../components/film_card.vue"
+import films from "../films.json"
+export default Vue.extend({
+  components: {
+    filmCard,
   },
-  data(){
-    return{
-      films:films.films
+  data() {
+    return {
+      films: films.films,
+      hover:false,
     }
-  }
-});
+  },
+  methods: {
+    goToFilm(film) {
+      this.hover=false;
+      this.$router.push({
+        name: "Film",
+        params: { filmId: film.id, filmSize: film.size.toString() },
+      })
+    },
+  },
+})
 </script>
+<style scoped>
+  .active{
+    transform:scale(0.9);
+    transition: transform .2s;
+  }
+  .active:hover{
+    transform: scale(1);
+  }
+  .active:focus{
+    filter: blur(5px) brightness(90%);
+  }
+</style>
