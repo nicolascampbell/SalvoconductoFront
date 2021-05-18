3d60ee93fe4c8693fd97ae5846c4885c8cc4e14a
nicolascampbell <nicodsm7@gmail.com>
2021-05-18 13:48:50 +0200

    just changed quotes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
-import router from "./router"
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
