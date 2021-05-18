dfd3ad7d796a3a217ae45268fde4d8b3e015846e
nicolascampbell <nicodsm7@gmail.com>
2021-05-18 13:50:12 +0200

    Just deleted some tests

--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="films_container" class="container">
-    <b-button :to="{ name: 'FilmList' }">
-          go Back! 
-    </b-button>
+    
      <b-row class="vh-100 text-center" align-v="center" v-for="image in filterImg(img, filmId)" :key="image.id" >
       <b-col  >
         <photo-card :pic="image"/>
       </b-col>
      </b-row>
+     <b-button :to="{ name: 'FilmList' }">
+          go Back! 
+    </b-button>
   </div>
 </template>
 <script lang="ts">
@@ -29,11 +30,9 @@ export default ({
   methods:{
     //this will be done in the future by the backend
     filterImg: function(imagesArray,filmid){
-      alert("reached");
       let images = imagesArray.filter(function (image){
         return image.id.includes(`${filmid}_`);
       });
-      console.log(images);
       return images;
     }
   }
