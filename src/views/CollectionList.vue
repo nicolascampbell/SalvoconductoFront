8f610eace82bb876d8ad36aee2e55736a75d8192
nicolascampbell <nicodsm7@gmail.com>
2021-06-01 10:05:44 +0200

    "under construction"

--- src/views/CollectionList.vue
+++ src/views/CollectionList.vue
@@ -1,31 +1,32 @@
 <template>
-  <div id="films_container" class="container">
-     <b-row class="vh-100 text-center" align-v="center" v-for="film in films" :key="film.id">
-      <b-col cols="2">
-        <b-button :to="{ name: 'Home' }">
-          go Back! 
-        </b-button>
+  <div class="container">
+    <b-row class="vh-100 text-center" align-v="center">
+      <b-col cols="6">
+        <div
+          style="
+            width: 100%;
+            height: 0;
+            padding-bottom: 100%;
+            position: relative;
+          "
+        >
+          <iframe
+            src="https://giphy.com/embed/Ti1dpOjRAGEbcf7cOG"
+            width="100%"
+            height="100%"
+            style="position: absolute"
+            frameBorder="0"
+            class="giphy-embed"
+          ></iframe>
+          
+        </div>
+        <h4>Working on it hihi</h4>
+        
       </b-col>
-      <b-col>
-        <film-card :filmInfo="film"/>
-      </b-col>
-      <b-col cols="1"></b-col>
-
-     </b-row>
+    </b-row>
   </div>
 </template>
 <script lang="ts">
-import filmCard from '../components/film_card.vue';
-import films from  "../films.json";
-export default ({
-  components:{
-      filmCard,
-  },
-  data(){
-    return{
-      films:films.films
-    }
-  }
-});
+export default {}
 </script>
 
