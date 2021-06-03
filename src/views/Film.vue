57a2113fee909457afa2c29f1e2c004ce3625bf4
nicolascampbell <nicodsm7@gmail.com>
2021-06-03 22:51:02 +0200

    fixed some stuff and made it ready for deploy

--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="films_container" class="container">
-    <b-row
-      id="rows"
-      class="vh-100 text-center"
-      align-v="center"
-    >
+    <b-row id="rows" class="vh-100 text-center" align-v="center">
       <b-col offset-lg="2" cols="8">
         <navigate-items @changeIndex="changeIndex" :maxItems="maxImg">
           <template v-slot:current>
-            <photo-card
-               key="current"
-               :pic="filterImg(img, filmId)[index]" />
+            <photo-card key="current" :pic="filterImg(img, filmId)[index]" />
           </template>
           <template v-slot:next>
-            <photo-card
-              key="next"
-              :pic="filterImg(img, filmId)[index]"
+            <photo-card key="next" :pic="filterImg(img, filmId)[index]"
           /></template>
         </navigate-items>
       </b-col>
@@ -26,7 +18,15 @@
 import photoCard from "../components/photo_card.vue"
 import images from "../images.json"
 import navigateItems from "../components/navigateItems.vue"
-export default {
+import Vue from "vue"
+
+type image={
+  title: String
+  id: String
+  description: String
+        
+}
+export default Vue.extend({
   components: {
     photoCard,
     navigateItems
@@ -44,17 +44,18 @@ export default {
   },
   methods: {
     //this will be done in the future by the backend
-    filterImg: function (imagesArray, filmid) {
+    filterImg: function (imagesArray, filmid):Array<image>{
       let images = imagesArray.filter(function (image) {
         return image.id.includes(`${filmid}_`)
       })
-      return images
+      return images;
     },
-    changeIndex(i: number) {
-      this.index = i
+    changeIndex(i: number):Number {
+      this.index = i;
+      return 0;
     },
   },
-}
+})
 </script>
 <style scoped>
 </style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    publicPath: '/Salvoconducto_front/'
+  }
