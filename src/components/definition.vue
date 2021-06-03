dcdc43fda111012590e1f8a731574481be8ed371
nicolascampbell <nicodsm7@gmail.com>
2021-06-03 02:01:43 +0200

    abstracted def component

---
+++ src/components/definition.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1 id="title" class="text-left">{{title}}</h1>
+    <h6 id="subtitle" class="text-left">{{subtitle}}</h6>
+    <p class="text-left">{{type}}</p>
+    <ol class="text-left">
+      <li v-for="defs in definitions"> {{defs}}</li>
+      <li v-if="withSlot"><slot></slot></li>
+    </ol>
+  </div>
+</template>
+<script lang="ts">
+export default {
+    components: {},
+    props:{
+        title:String,
+        subtitle:String,
+        type: String,
+        definitions: Array,
+        withSlot:Boolean
+    },
+}
+</script>
+<style>
+#title {
+  color: rgb(71, 70, 70);
+  display: inline-block;
+}
+#subtitle,
+p {
+  color: rgb(109, 106, 106);
+}
+ol li::marker {
+  color: rebeccapurple;
+  font-size: 0.83em;
+  line-height: 0.5em;
+  vertical-align: baseline;
+  position: relative;
+  top: -0.4em;
+  font-weight: bold;
+}
+li {
+  color: rgb(61, 61, 61);
+  margin: 0 0 10px 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,34 @@
 <template>
-  <div>
-    <b-container style="position: relative">
-      <b-row class="vh-100" align-v="center">
-        <b-col>
-        </b-col>
-        <b-col>
-          <h1 id="title" class="text-left">Salvoconducto</h1>
-          <h6 id="subtitle" class="text-left">[Sal·vo·con·duc·to]</h6>
-          <p class="text-left">Website</p>
-          <ol class="text-left">
-            <li>A place to share what my eyes see.</li>
-            <li>Within can be found all my films and collections.</li>
-            <li>Freedom to do something without fear of punishment.</li>
-            <li><about-me /></li>
-          </ol>
-        </b-col>
-        <b-col></b-col>
-      </b-row>
-    </b-container>
-    
-  </div>
+  <b-container id="container">
+    <b-row class="vh-100" align-v="center">
+      <b-col> </b-col>
+      <b-col>
+        <definition
+          :title="'Salvoconducto'"
+          :subtitle="'[Sal·vo·con·duc·to]'"
+          :type="'Website'"
+          :definitions="['A place to share what my eyes see.',
+                          'Within can be found all my films and collections.',
+                          'Freedom to do something without fear of punishment.']"
+          :withSlot="true">
+          <about-me />
+        </definition>      
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+  </b-container>
 </template>
 <script lang="ts">
 import aboutMe from "../components/about_me_modal.vue"
+import definition from "../components/definition.vue"
+
 export default {
   components: {
     aboutMe,
+    definition,
   },
 }
 </script>
 <style>
-#title {
-  color: rgb(71, 70, 70);
-  display: inline-block;
-}
-#subtitle,
-p {
-  color: rgb(109, 106, 106);
-}
-ol li::marker {
-  color: rebeccapurple;
-  font-size: 0.83em;
-  line-height: 0.5em;
-  vertical-align: baseline;
-  position: relative;
-  top: -0.4em;
-  font-weight: bold;
-}
-li {
-  color: rgb(61, 61, 61);
-  margin: 0 0 10px 0;
-}
+
 </style>
