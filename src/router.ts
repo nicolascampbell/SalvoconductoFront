a329f73d55d1b6385c43e971e58d8be7c588f84b
nicolascampbell <nicodsm7@gmail.com>
2021-06-22 17:40:41 +0200

    new path name

--- src/router.ts
+++ src/router.ts
@@ -26,7 +26,7 @@ export default new Router({
     component: CollectionList
   },
   {
-    path: "/Films/:filmId",
+    path: "/Films/:filmid",
     name: "Film",
     component: Film,
     props:true,
