9bbbbc9bfb678b7b838301301b40792ea6fd3fdf
nicolascampbell <nicodsm7@gmail.com>
2021-06-22 17:39:44 +0200

    Skeleton of new view for films for now randomized with relevance

--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,15 +1,24 @@
 <template>
   <b-container id="films_container" >
-    <b-row id="rows" class="vh-100 text-center" align-v="center">
-      <b-col offset-lg="2" cols="8">
-        <navigate-items @changeIndex="changeIndex" :maxItems="maxImg">
-          <template v-slot:current>
-            <photo-card key="current" :pic="filterImg(img, filmId)[index]" />
-          </template>
-          <template v-slot:next>
-            <photo-card key="next" :pic="filterImg(img, filmId)[index]"
-          /></template>
-        </navigate-items>
+    <b-row class="vh-100" align-v="center">
+      <b-col offset="1" offset-lg="6">
+        <definition
+            :title="`Film ${filmInfo.id}`"
+            :subtitle="'[Film]'"
+            :type="'Chronicle'"
+            :definitions="[
+              `Taken on ${filmInfo.year}`,
+              `Taken in ${filmInfo.place}`,
+              filmInfo.description,
+            ]"
+            :withSlot="false"
+          >
+          </definition>
+      </b-col>
+    </b-row>
+    <b-row id="rows" >
+      <b-col class="image" :offset="Math.round(image.relevance/4+Math.random()*2)" :cols="image.relevance*2+5" v-for="image in filterImg(img, filmInfo.id)" :key="image.id">
+        <photo-card key="current" :pic="image" />
       </b-col>
     </b-row>
   </b-container>
@@ -17,32 +26,52 @@
 <script lang="ts">
 import photoCard from "../components/photo_card.vue"
 import images from "../images.json"
-import navigateItems from "../components/navigateItems.vue"
+import definition from "../components/definition.vue"
+
 import Vue from "vue"
 
 type image={
   title: String
   id: String
   description: String
-        
+  relevance:Number  
+}
+type film = {
+  id: String,
+  description: String,
+  size: Number,
+  year: String,
+  place: String,
+  tags: Array<String>,
+  fotoCover: Number
 }
 export default Vue.extend({
   components: {
     photoCard,
-    navigateItems
+    definition,
   },
   props: {
-    filmId: String,
-    filmSize: Number,
+    filmInfo: {
+      type: Object as () => film,
+    }
   },
   data() {
     return {
-      index:0,
-      maxImg:this.filmSize-1,
       img: images.images,
     }
   },
   methods: {
+    compare:function (a:image, b:image) {
+      if (a.relevance<b.relevance) {
+        return -1;
+      }
+      if (a.relevance>b.relevance) {
+        return 1;
+      }
+      // a must be equal to b
+      return 0;
+    }
+    ,
     //this will be done in the future by the backend
     filterImg: function (imagesArray, filmid):Array<image>{
       let images = imagesArray.filter(function (image) {
@@ -50,12 +79,12 @@ export default Vue.extend({
       })
       return images;
     },
-    changeIndex(i: number):Number {
-      this.index = i;
-      return 0;
-    },
   },
 })
 </script>
 <style scoped>
+.image{
+  margin-bottom: 20px;
+}
+
 </style>
