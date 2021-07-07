35c5590bdfdf79625dcba1eb3eac452259f48586
nicolascampbell <nicodsm7@gmail.com>
2021-07-07 14:46:59 +0200

    Started to work on it

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,24 +1,27 @@
 <template >
   <div id="navtoken">
-    <b-container id="nav" v-if="open">
-      <b-row>
-        <b-col cols="2" class="item">
-          <b-link to="/"> Home </b-link>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="2" class="item">
-          <b-link to="/Films"> Films </b-link>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="2" class="item">
-          <b-link class="item" to="/Collections"> Collections </b-link>
-        </b-col>
-      </b-row>
-    </b-container>
-    <div id="token" v-else @click="open=!open">
-    </div>
+    <transition name="appear">
+      <b-container id="nav" v-if="open">
+        <b-row>
+          <b-col cols="2" class="item">
+            <b-link to="/"> Home </b-link>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="2" class="item">
+            <b-link to="/Films"> Films </b-link>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="2" class="item">
+            <b-link class="item" to="/Collections"> Collections </b-link>
+          </b-col>
+        </b-row>
+      </b-container>
+      </transition>
+
+      <div id="token" v-if="!open" @click="open=!open">
+      </div>
   </div>
 </template>
 <script lang="ts">
@@ -33,14 +36,10 @@ export default Vue.extend({
 })
 </script>
 <style scoped>
-#navtoken{
-  width:150px;
-  height: 100px;
-}
+
 #nav {
   text-align: center;
   background-color: grey;
-
 }
 #token{
   width:10%;
@@ -54,5 +53,16 @@ export default Vue.extend({
 }
 .item {
   padding-bottom: 10px;
+}
+.appear-enter-active {
+  transition: all .3s cubic-bezier(.68,-0.55,.27,1.55);
+}
+.appear-leave-active {
+  transition: all .3s cubic-bezier(.68,-0.55,.27,1.55);
+}
+.appear-enter, .appear-leave-to
+/* .app-enter-active-leave-active below version 2.1.8 */ {
+  transform: translateX(-100px);
+  opacity: 0;
 }
 </style>
