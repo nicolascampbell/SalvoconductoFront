4e8483e95ba61aa49feb362cc784796a1ea579bf
nicolascampbell <nicodsm7@gmail.com>
2021-07-09 17:58:29 +0200

    added grey background in the films

--- src/views/FilmList.vue
+++ src/views/FilmList.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container id="films_container" fluid >
-    <b-row class="vh-100"  id="firstRow" align-v="center" no-gutters>
+    <b-row class="vh-100" id="firstRow" align-v="center" no-gutters>
       <b-col offset="1" offset-lg="9">
         <definition
           :title="'Films'"
@@ -15,14 +15,14 @@
         </definition>
       </b-col>
     </b-row>
-    <b-row no-gutters style=" text-align: center;">
+    <b-row no-gutters class="row" style=" text-align: center;">
       <b-col id="pil" offset="10" cols="2" @click="changeOrder">
            Date 
           <b-icon-arrow-down font-scale="0.7" v-if="asc"></b-icon-arrow-down>
           <b-icon-arrow-up font-scale="0.7" v-else></b-icon-arrow-up>
       </b-col>
     </b-row>
-    <b-row v-for=" film in films" :key="film.id" no-gutters>
+    <b-row class="row" v-for=" film in films" :key="film.id" no-gutters>
       <b-col  offset-lg="3" lg="6"
               offset-sm="2" sm="8"
               offset="1" cols="10">
@@ -64,10 +64,17 @@ export default Vue.extend({
 <style scoped>
 #films_container{
   padding:0px;
-  
+  background-color:rgba(219, 213, 213, 0.699);
+}
+.row:last-of-type{
+  margin-bottom: 0!important;
 }
 #films_container>*{
   margin-bottom: 5%;
+}
+#firstRow{
+  background-color:white;
+  border-bottom: black solid 1px;
 }
 #pil{
   background-color:rgba(165, 74, 226, 0.829);
