5f1fcb213bfd456bcd47e5150a287f2bd84f4e79
nicolascampbell <nicodsm7@gmail.com>
2021-07-10 22:51:01 +0200

    click and scroll hide+animation hide

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template >
-  <div id="navtoken">
+  <div id="navtoken" >
     <transition name="appear">
-      <div id="navbar" v-if="open">
+      <div id="navbar" :class="{padd:this.$route.name!='Home'}" v-if="open">
         <b-container id="menu" >
           <b-row >
             <b-col class="item" @click="$router.push({name:'CollectionList'})" >
@@ -19,12 +19,14 @@
         </div>
       </div>
       </transition>
-      <div class="back" v-if="this.$route.name!='Home'&&!open" @click="$router.go(-1)">
+      <div class="back" v-if="this.$route.name!='Home'" @click="$router.go(-1)" >
         <b-icon-arrow-return-left id="arrow"></b-icon-arrow-return-left>
       </div>
-      <div class="token" v-if="!open" @click="open=!open">
-        <b-icon-arrow-right id="arrow"></b-icon-arrow-right>
-      </div>
+      <transition name="appear">
+        <div class="token" v-if="!open" @click.stop="open=!open" >
+          <b-icon-arrow-right id="arrow"></b-icon-arrow-right>
+        </div>
+      </transition>
        
   </div>
 </template>
@@ -39,9 +41,38 @@ export default Vue.extend({
   data() {
     return {
       open: false,
+      lastScrollPosition: 0,
     }
   },
-  methods: {},
+  methods: {
+    handleScroll: function () {
+      //For now this method may be unecessary because we want to close it always
+      const currentScrollPosition =
+        window.pageYOffset || document.documentElement.scrollTop;
+      // Because of momentum scrolling on mobiles,
+      //we shouldn't continue if it is less than zero
+      if (currentScrollPosition < 30) {
+        return;
+      }
+      // Here we determine whether we need to show or hide the navbar
+        this.$data.open=false;
+      
+      // Set the current scroll position as the last scroll position
+      this.lastScrollPosition = currentScrollPosition;
+    },
+    documentClick:function(){
+      this.open = false;
+        
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+    document.addEventListener('click', this.documentClick)
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+    document.addEventListener('click', this.documentClick)
+  },
 })
 </script>
 <style scoped>
@@ -50,9 +81,10 @@ export default Vue.extend({
   text-align: center;
   background-color: rgb(255, 255, 255);
   border: black solid 1px;
-  border-right: 0.5px solid;
   overflow-x: hidden;
   font-size:1.2em;
+  border-left: transparent;
+
 }
 
 .item:hover{
@@ -69,10 +101,12 @@ export default Vue.extend({
 }
 .mcap{
     background-color: rebeccapurple;
-    min-width: 3%;
-    height: 40%;
+    min-width: 0.7em;
+    height: 45%;
     border: black solid 1px;  
     cursor: pointer;
+    border-left: transparent;
+
 }
 .mcap:hover{
   background-color: rgb(142, 97, 187);
@@ -82,21 +116,29 @@ export default Vue.extend({
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
+    z-index: 1;
+
+}
+.padd{
+  left:2em;
+
 }
 .back{
-  width:8%;
+  min-width:2em;
   height:100%;
   border: black solid 1px;  
   cursor: pointer;
   border-left: transparent;
   background-color: #fae844f8;
+  z-index: 2;
 }
 .back:hover{
     background-color: rgba(235, 223, 123, 0.767);
 }
 .token{
   width:7%;
-  height:90%;
+  min-width:1.8em;
+  height:85%;
   background-color: rgba(115, 54, 177, 0.753);
   border: black solid 1px;  
   cursor: pointer;
@@ -115,14 +157,14 @@ export default Vue.extend({
   transform: translateY(-50%);  
 }
 .appear-enter-active {
-  transition: all .3s cubic-bezier(.68,-0.55,.27,1.55);
+  transition: all 0.8s ease-in;
 }
 .appear-leave-active {
-  transition: all .3s cubic-bezier(.68,-0.55,.27,1.55);
+  transition: all 0.5s ease-out;
 }
 .appear-enter, .appear-leave-to
 /* .app-enter-active-leave-active below version 2.1.8 */ {
-  transform: translateX(-100px);
-  opacity: 0;
+  transform: translateX(-100%);
+  opacity:0.8;
 }
 </style>
