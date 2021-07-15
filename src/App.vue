bcdb89ec90d35b59ace4cd8ac03c25315fd6cb8e
nicolascampbell <nicodsm7@gmail.com>
2021-07-15 22:59:34 +0200

    new position for navbar in small screen

--- src/App.vue
+++ src/App.vue
@@ -29,14 +29,32 @@ export default Vue.extend({
 #container {
   font-family: "Courier New", Courier, monospace;
 }
-#navbar{
-  top:5%;
-  width:30%;
-  min-width: 10em;
-  height: 15%;
-  position:fixed;
-  z-index: 100  ;
-
+@media screen and (min-width: 768px) {
+  #navbar{
+    top:5%;
+    width:25em;
+    height: 6em;
+    position:fixed;
+    z-index: 100  ;
+  }
 }
+@media screen and (max-width: 768px) {
+  #navbar{
+    top:50%;
+    right:0px;
+     width:25em;
+    height: 10em;
+    position:fixed;
+    z-index: 100  ;
+    display:flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+    z-index: 1;
+  }
+}
+
+
+
 
 </style>
