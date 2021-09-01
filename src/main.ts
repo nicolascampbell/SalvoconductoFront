2b4e572c095d201b5818344ee322a60b082494b5
nicolascampbell <nicodsm7@gmail.com>
2021-09-01 03:12:49 +0200

    imported the important plugins from bootstrap vue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+import { LayoutPlugin,ImagePlugin,ModalPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+Vue.use(LayoutPlugin);
+Vue.use(ImagePlugin);
+Vue.use(ModalPlugin);
+
+
 
 new Vue({
   router,
