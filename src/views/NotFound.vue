e375aa8fa4b7714681496c5012065bbd7616e7a5
nicolascampbell <nicodsm7@gmail.com>
2021-09-06 19:34:41 +0200

    Added NotFound  as landing page for 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container id="container">
+    <b-row class="vh-100" align-v="center">
+      <b-col lg="5" offset-lg="4"
+              md="9" offset-md="3"
+              sm="9" offset-sm="2">
+        <definition
+          :title="'404'"
+          :subtitle="'[4 0 4]'"
+          :type="'Error'"
+          :definitions="[
+            'Seems like you came to the wrong place',
+            'Do you ever feel like you are always at the wrong place at the wrong time?',
+          ]"
+          :withSlot="false"
+        >
+        </definition>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script lang="ts">
+import definition from "../components/definition.vue"
+
+export default {
+  components: {
+    definition,
+  },
+}
+</script>
+<style>
+
+</style>
