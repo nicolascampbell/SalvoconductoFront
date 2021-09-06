c238d71fe62c4eb9dbf78d2b45de437cebd4fe8c
nicolascampbell <nicodsm7@gmail.com>
2021-09-06 19:35:06 +0200

    created catch all route for routes that are not defined

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ const FilmList = () => import('./views/FilmList.vue');
 const CollectionList = () => import('./views/CollectionList.vue');
 const Film = () => import('./views/Film.vue');
 const Collection = () => import('./views/Collection.vue');
+const NotFound = () => import('./views/NotFound.vue');
 
 Vue.use(Router);
 
@@ -35,8 +36,14 @@ export default new Router({
     path: "/Collections/:collectionId",
     name: "Collection",
     component: Collection
+  },
+  {
+    path: '/:catchAll(.*)', 
+    name: "NotFound",
+    component: NotFound
   },],
   scrollBehavior (to, from, savedPosition) {
+    //So that when you go to another route it shows you the start of the page
     return { x: 0, y: 0 }
   }
   
