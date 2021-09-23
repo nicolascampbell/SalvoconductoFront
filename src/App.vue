9259dba196e36db6ae1345541f04220e24cc6eb0
nicolascampbell <nicodsm7@gmail.com>
2021-09-23 10:38:40 +0200

    changed position of navbar in phones

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ export default Vue.extend({
     right:0px;
     width:70%;
     height: 10em;
-    position:fixed;
+    position:absolute;
     z-index: 100  ;
     display:flex;
     flex-direction: column;
