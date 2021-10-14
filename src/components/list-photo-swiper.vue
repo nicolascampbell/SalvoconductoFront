0fe9af91672f7d5070ac05db7ab9de5f48a70b54
nicolascampbell <nicodsm7@gmail.com>
2021-10-14 16:38:32 +0200

    abstracted list of photos from FIlm view

---
+++ src/components/list-photo-swiper.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="images-container-swiper">
+    <swiper @progress="progress" class="swiper" :options="swiperOption">
+      <swiper-slide 
+        v-for="photo in photos"
+        :key="photo.index">
+        <photo-card :photo="photo" class="swiper-lazy"/>
+        <div class="swiper-lazy-preloader swiper-lazy-preloader-black"></div>
+
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div id="prev"></div>
+      <div id="next"></div>
+      <div v-if="navigation" class="swiper-button-next swiper-button-black" slot="button-next"></div>
+      <div v-if="navigation" class="swiper-button-prev swiper-button-black" slot="button-prev"></div>
+    </swiper>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper"
+import "swiper/css/swiper.css"
+import photoCard from "./card-photo.vue"
+import Vue from "vue"
+
+export default Vue.extend({
+  name:'listPhotoSwiper',
+  components: {
+    Swiper,
+    SwiperSlide,
+    photoCard,
+  },
+  directives: {
+    swiper: directive,
+  },
+  props:{
+    photos:Array,
+    navigation:Boolean
+  },
+  data() {
+    return {
+      swiperOption: {
+        lazy: true,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "progressbar",
+        },
+         keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+        },
+      },
+    }
+  },
+  methods:{
+    //this method gets called everytime I pass to new photo in galery
+    progress:function(progress){
+      let a =document.getElementsByClassName("swiper-pagination-progressbar-fill")[0];
+      if(progress>=1){
+        a.classList.add("swiper-pagination-progressbar-filled")
+        //We take the style with yellow bar only if its there
+      }else if(a.classList.contains("swiper-pagination-progressbar-filled")){
+        a.classList.remove("swiper-pagination-progressbar-filled")
+      }
+    }
+  },
+  created(){
+    if(this.navigation){
+      this.swiperOption["navigation"] =
+      {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    }
+  }
+})
+</script>
+
+<style>
+#images-container-swiper {
+  height: 100vh;
+  width: 100%;
+}
+.swiper-pagination-progressbar-fill{
+  background:var(--purple)!important;
+}
+.swiper-pagination-progressbar-filled{
+  background:var(--yellow)!important;
+}
+.swiper-lazy-preloader{
+  --swiper-preloader-color:var(--purpleHover);
+}
+
+.swiper {
+  height:100%;
+  width:100%;
+}
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+}
+</style>
--- src/views/Film.vue
+++ src/views/Film.vue
@@ -2,7 +2,7 @@
   <div id="film" v-if="loadedSources">
     <b-container id="film-container" fluid>
       <b-row class="vh-100" align-v="center">
-        <b-col offset="1" offset-md="3" offset-xl="7" xl="3" >
+        <b-col offset="1" offset-md="3"  offset-xl="6" xl="5" >
           <definition
             :title="`Film ${film._id}`"
             :subtitle="'[Film]'"
@@ -17,24 +17,22 @@
           </definition>
         </b-col>
       </b-row>
-      <b-row >
-        <div
-          id="images-container"
-        >
-          <div
-            :class="{'small-image':photo.relevance<=1,'image':true}"
-            v-for="photo in film.photos"
-            :key="photo.index"
-            @click="changeIndex(photo)"
-            :style="`width:${getPhotoWidth(photo.relevance)}%!important;`"
-            >
-            <photo-card 
-            :photo="photo"/>
-          </div>
-          
-        </div>
+      <b-row v-if="!isMobile()" no-gutters  style="text-align: center">
+        <b-col 
+          offset="4"
+          cols="4"
+          offset-sm="5"
+          sm="2">
+          <button-toggle-view @changeView="desktopView=!desktopView" label="Change View" />
+        </b-col>
       </b-row>
-      <b-row  class="row" align-v="center" style=" margin: 5%;">
+      <b-row align-v="center" style="margin-top:20px">
+        <b-col >
+          <list-photo-swiper v-if="isMobile()||!desktopView" :photos="film.photos" :navigation="!isMobile()&&!desktopView"/>
+          <list-photo-desktop v-else :photos="film.photos"/>
+        </b-col>
+      </b-row>
+      <b-row v-if="!isMobile()&&desktopView" class="row" align-v="center" style=" margin: 5%;">
         <b-col
           offset="4"
           cols="4"
@@ -50,17 +48,21 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-import photoCard from '../components/photo-card.vue'
 import definition from '../components/definition.vue'
 import { Film, Photo } from '@/customTypes'
 import btnScrollTop from '../components/button-scroll-top.vue'
+import ListPhotoDesktop from '../components/list-photo-desktop.vue'
+import listPhotoSwiper from '@/components/list-photo-swiper.vue'
+import buttonToggleView from '../components/button-toggle-view.vue'
 
 export default Vue.extend({
   name:'Film',
   components: {
-    photoCard,
     definition,
-    btnScrollTop
+    btnScrollTop,
+    ListPhotoDesktop,
+    listPhotoSwiper,
+    buttonToggleView
   },
   data() {
     return {
@@ -69,44 +71,14 @@ export default Vue.extend({
       },
       index:-1,
       loadedSources:false,
-      isNotPhone:true,
+      desktopView:true
     }
   },
   methods:{
-    changeIndex:function(photo:Photo){   
-      this.isNotMobile();
-      if(this.isNotPhone){
-        this.index=photo.index.valueOf()-1;
-      }
+    // Checks if we are on mobile or not.
+    isMobile:function(){
+      return window.matchMedia("(max-width: 767px)").matches;
     },
-    //Not so useful without imageviewer! we wanna use this modal component if the device is not a phone.
-    //in phones is just not worth it.
-    isNotMobile:function(){
-      this.isNotPhone= !window.matchMedia("(max-width: 767px)").matches;
-    },
-    getPhotoWidth(relevance){
-      if(!this.isNotPhone){
-        return 100;
-      }
-      if(relevance==0){
-        return 30;
-      }else if(relevance==1){
-        return 45;
-      }else if(relevance==2){
-        return 70;
-      }else{
-        return 100;
-      }
-    },
-    getRandomMargin(relevance){
-     if(relevance<=1){
-        return Math.random()*20;
-      }
-    }
-    
-  },
-  mounted(){
-    this.isNotMobile();
   },
   async beforeMount() {
     try {
@@ -132,36 +104,12 @@ export default Vue.extend({
     padding-right: 10%;
     padding-left: 10%;
   }
-  #images-container{
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-around;
-    row-gap: 30vh;
-    column-gap: 30px;
-    width:100%;
-  }
 
-  .small-image:nth-child(3n){
-    margin-top:100px;
-  }
-  .small-image:nth-child(5n){
-    margin-right:auto;
-  }
 }
 @media screen and (max-width: 767px) {
   #film-container{
     padding-right: 7%;
     padding-left: 7%;
-  }
-  #images-container{
-    display: flex;
-    flex-flow: column wrap;
-    justify-content: center;
-    row-gap: 50vh;
-    width:100%;
-  }
-  .image{
-    width:100%!important;
   }
 }
 @media screen and (max-width: 767px) and (orientation : landscape) {
@@ -169,7 +117,6 @@ export default Vue.extend({
     padding-right: 20%;
     padding-left: 20%;
   }
-  
 }
 
 
