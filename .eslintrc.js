d40bafbe3a2faeb7741e60d26bc9a43a59e4fbec
nicolascampbell <nicodsm7@gmail.com>
2021-11-01 15:40:00 +0100

    [ADD] Eslint plugin

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  "parser": "vue-eslint-parser",
+  parserOptions: {
+    "parser": "@typescript-eslint/parser",
+    "sourceType" :"module"
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
